f39cfea6fb54ef772b1fbb9e3defe35892d92b91
padmaavathikt <31005321+padmaavathikt@users.noreply.github.com>
2017-08-18 21:10:48 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,16 +1,9 @@
-console.log('Loaded!');
+var button = document.getElementById("counter");
+var counter = 0;
 
-var element = document.getElementById('main-text');
-element.innerHTML = "New Value!!!";
-
-var img = document.getElementById("madi");
-var marginLeft = 0;
-function moveRight () {
-  marginLeft = marginLeft + 1;
-  img.style.marginLeft = marginLeft + 'px';
-}
-
-img.onclick = function () {
-    //img.style.marginLeft = '100px';
-  var interval = setInterval(moveRight, 50);
+button.onclick = function () {
+  
+  counter = counter + 1;
+  var span = document.getElementById("count");
+  span.innerHTML = counter.toString();
 };
