d5b02e0410a3a4346da1cc011400021b82505a4e
padmaavathikt <31005321+padmaavathikt@users.noreply.github.com>
2017-08-18 17:27:49 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,9 @@
 console.log('Loaded!');
+
+var element = document.getElementById('main-text');
+element.innerHTML = "New Value!!!";
+
+var img = document.getElementById("madi");
+img.onclick = function () {
+  img.style.marginLeft = '100px';  
+};
