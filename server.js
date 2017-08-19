20bf892662f990bca3c716812f1abc39dc2dd5b0
padmaavathikt <31005321+padmaavathikt@users.noreply.github.com>
2017-08-19 12:16:13 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -117,7 +117,7 @@ app.get('/ui/madi.png', function (req, res) {
 var names = [];
 app.get('/submit-name/:name', function (req, res){
    var name = req.params.name;
-   
+   names.push(name);
    res.send(JSON.stringify(names));
 });
 // Do not change port, otherwise your app won't run on IMAD servers
