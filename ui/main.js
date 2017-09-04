6fedf1845a160545b6b6ef53fdb5273c26d241f1
padmaavathikt <31005321+padmaavathikt@users.noreply.github.com>
2017-09-04 17:04:36 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -48,7 +48,8 @@ loginBtn.onclick = function () {
    
    //submit input value
     var username = document.getElementById("username").value;
-    var password = document.getElementById("password").value;
+    var password = 'password';
+    //document.getElementById("password").value;
    
    console.log(username);
    console.log(password);
